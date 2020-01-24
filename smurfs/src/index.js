8c62241f5bb5ec8f38d5d6eb97853fc87affb329
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-24 08:44:46 -0800

    imported files to index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//import { reducer } from './reducers';
+
+const store = createStore( applyMiddleware(thunk));
+
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
